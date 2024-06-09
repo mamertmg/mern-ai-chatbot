7fd184d617599c2f78fd354911bbbe7b368ea287
mamertmg <marquezgarcia.a@gmail.com>
2024-06-09 15:11:24 +0200

    add routes frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { createTheme, ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Roboto Slab,serif",
+    allVariants: { color: "white" },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <Toaster position="top-right" />
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
   </React.StrictMode>,
 )
