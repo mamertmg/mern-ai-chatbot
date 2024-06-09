3246354db4a152694664e276ff8512f2120c21a3
mamertmg <marquezgarcia.a@gmail.com>
2024-06-09 15:40:39 +0200

    add context

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Logo from "./shared/Logo";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <AppBar sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}>
+      <Toolbar sx={{ display: "flex" }}>
+        <Logo />
+        <div>
+          
+        </div>
+      </Toolbar>
+    </AppBar>
   )
 }
 
